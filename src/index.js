2b3c16f36f9e8a887a7e760c9a94c6d2f4981ebb
Miguel Pardo <migsub77@gmail.com>
2018-03-03 22:37:24 -0800

    deleting and organizing project some more! This thing is broke as heck!

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactRouter from './components/initial/ReactRouter';
-
-ReactDOM.render(
-  <ReactRouter />,
-  document.getElementById('root')
-);
