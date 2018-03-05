64fe08cf399d2d5ef84098ffbcfa9564e205006a
Miguel Pardo <migsub77@gmail.com>
2018-03-04 16:56:37 -0800

    removed unnecessary files, fixed invite api call, slack app styling and integration done

--- src/js/api/slackApp.js
+++ src/js/api/slackApp.js
@@ -1,20 +1,27 @@
 import axios from 'axios';
 
+$('.slack-invite-form').on('submit', function(e) {
+  e.preventDefault();
+  const target = e.target;
+  const userEmail = target.email.value;
+  slackInviteEmail(userEmail);
+})
+
 // send email to invite
-export function slackInviteEmail(email) {
+function slackInviteEmail(email) {
   const environment = process.env.NODE_ENV;
+  console.log(environment);
   let endpoint = 'http://localhost:8000/api/slack/invite';
   if(environment !== 'dev') {
     endpoint = 'https://still-spire-83012.herokuapp.com/api/slack/invite';
   }
-  console.log(email);
-    axios.post(endpoint, {email: email})
-    .then(res => {
-      alert(res.data.error || 'Check Your Email!');
-    })
-    .catch(err => {
-      console.log(err);
-      alert(err);
+  axios.post(endpoint, {email: email})
+  .then(res => {
+    alert(res.data.error || 'Check Your Email!');
+  })
+  .catch(err => {
     console.log(err);
-    });
+    alert(err);
+    console.log(err);
+  });
 }
--- src/js/global/toggleElements.js
+++ src/js/global/toggleElements.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
   $('.hamburger, .nav-item').on('click', function() {
     $('.nav-body').toggleClass('show-nav');
   });
+  $('.slack-btn, .popup-modal').on('click', function() {
+    $('.popup-modal, .slack-app').fadeToggle();
+  });
 });
