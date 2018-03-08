b57449bd998d905c95760f79a881b1076d83b02a
Miguel Pardo <migsub77@gmail.com>
2018-03-07 23:30:42 -0800

    added api call to wake my server

--- src/js/api/slackApp.js
+++ src/js/api/slackApp.js
@@ -1,5 +1,19 @@
 import axios from 'axios';
 
+// This will wake up the heroku server which takes a few seconds to start up.
+// I only did this because its my own personal server on the free plan
+$('.slack-btn').on('click', function() {
+  axios.get('https://still-spire-83012.herokuapp.com/api/wakeme')
+  .then(res => {
+      console.log(res.data);
+  })
+  .catch(err => {
+    console.log(err);
+  });
+});
+
+
+// gathers form data (email address) and sends to server to access the slack api for an invite
 $('.slack-invite-form').on('submit', function(e) {
   e.preventDefault();
   const target = e.target;
@@ -10,7 +24,6 @@ $('.slack-invite-form').on('submit', function(e) {
 // send email to invite
 function slackInviteEmail(email) {
   const environment = process.env.NODE_ENV;
-  console.log(environment);
   let endpoint = 'http://localhost:8000/api/slack/invite';
   if(environment !== 'dev') {
     endpoint = 'https://still-spire-83012.herokuapp.com/api/slack/invite';
